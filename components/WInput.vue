9fb889311baa3071572d6c5c288212da26732e68
tim760255458 <760255458@qq.com>
2018-10-08 20:46:10 +0800

    更新input组件

--- components/WInput.vue
+++ components/WInput.vue
@@ -1,13 +1,25 @@
 <template>
-    <input :value="value" @input="handleInput" @change="handleChange" @focus="handleFocus" @blur="handleBlur" :type="type">
+    <input :class="classes" :style="{ width: `${this.width}px` }" :value="value" :placeholder="placeholder" @input="handleInput" @change="handleChange" @focus="handleFocus" @blur="handleBlur" :type="type">
 </template>
 
 <script>
+const baseCls = 'w-input'
 export default {
     props: {
-        value: {},
+        value: {
+            type: String
+        },
         type: {
             default: 'text'
+        },
+        placeholder: {
+            type: String
+        },
+        line: {
+            type: Boolean
+        },
+        width: {
+            type: Number
         }
     },
     data () {
@@ -15,18 +27,32 @@ export default {
             
         }
     },
+    computed: {
+        classes () {
+            return [
+                `${baseCls}`,
+                {
+                    [`${baseCls}-type-${this.type}`]: this.type === 'textarea',
+                    [`${baseCls}-line`]: this.line
+                }
+            ]
+        }
+    },
     methods: {
-        handleInput (event) {
-            console.log('handleInput')
+        handleInput (event) { // 输入中触发的事件
+            // console.log('handleInput')
             this.$emit('on-input', event)
         },
-        handleChange (event) {
+        handleChange (event) { // 输入框失去焦点且数据与上次不同时触发的事件
+            // console.log('handleChange')
             this.$emit('on-change', event)
         },
-        handleFocus (event) {
+        handleFocus (event) { // 输入框获取焦点时触发的事件
+            // console.log('handleFocus')
             this.$emit('on-focus', event)
         },
-        handleBlur (event) {
+        handleBlur (event) { // 输入框失去焦点时触发的事件
+            // console.log('handleBlur')
             this.$emit('on-blur', event)
         }
     }
@@ -34,5 +60,13 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+.w-input {
+    box-sizing border-box
+}
+.w-input-type-textarea {
 
+}
+.w-input-line {
+
+}
 </style>
